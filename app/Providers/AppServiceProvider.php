95de6b21f693c0322085a8e4300ba331dc1a7a40
Stanislav Chistenko <skvest1@gmail.com>
2019-05-21 14:21:38 +0600

    Added custom PersistentDatahandler for Facebook SDK

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Facebook\PersistentData\PersistentDataInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,7 +17,20 @@ class AppServiceProvider extends ServiceProvider
         $fb = new \Facebook\Facebook([
             'app_id' => config('services.facebook.id'),
             'app_secret' => config('services.facebook.secret'),
-            'default_graph_version' => 'v3.3'
+            'default_graph_version' => 'v3.3',
+            'persistent_data_handler' => new class() implements PersistentDataInterface {
+                private $sessionPrefix = 'FB_';
+
+                public function get($key)
+                {
+                    return \Session::get($this->sessionPrefix . $key);
+                }
+
+                public function set($key, $value)
+                {
+                    \Session::put($this->sessionPrefix . $key, $value);
+                }
+            },
         ]);
         $this->app->instance('Facebook\SDK', $fb);
     }
